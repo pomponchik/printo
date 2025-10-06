91ee98e6e64c5df669062397599a09c51f14fbbe
esblinov <esblinov@avito.ru>
2025-10-06 17:20:56 +0300

    Simplify superrepr lambda handling

--- printo/reprs.py
+++ printo/reprs.py
@@ -5,12 +5,11 @@ from inspect import isfunction
 
 def superrepr(value: Any) -> str:
     if isfunction(value):
-        if hasattr(value, '__name__'):
-            result = value.__name__
+        result = value.__name__
 
-            if result == '<lambda>':
-                return 'λ'
+        if result == '<lambda>':
+            return 'λ'
 
-            return result
+        return result
 
     return repr(value)
