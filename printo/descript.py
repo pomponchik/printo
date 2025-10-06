9fe40c4592716063e3d91fde51d9e5594e4c7a64
esblinov <esblinov@avito.ru>
2025-10-06 13:11:22 +0300

    Standardize args/kwargs serialization in descript_data_object

--- printo/descript.py
+++ printo/descript.py
@@ -10,17 +10,15 @@ def descript_data_object(
     args_description_chunks = []
     for argument in args:
         args_description_chunks.append(serializator(argument))
-    args_description: str = ", ".join(args_description_chunks)
+    args_description: str = ', '.join(args_description_chunks)
 
-    kwargs_description: str = ", ".join(
-        [
-            f"{argument_name}=" + serializator(value)
-            for argument_name, value in kwargs.items()
-        ]
-    )
+    kwargs_description = []
+    for argument_name, value in kwargs.items():
+        kwargs_description.append(f"{argument_name}=" + serializator(value))
+    kwargs_description: str = ', '.join(kwargs_description_chunks)
 
     breackets_content = ", ".join(
         [x for x in (args_description, kwargs_description) if x]
     )
 
-    return f"{class_name}({breackets_content})"
+    return f'{class_name}({breackets_content})'
