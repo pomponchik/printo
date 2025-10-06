4e14f47ac9f14773d5cb32fb1bd28b237049381a
esblinov <esblinov@avito.ru>
2025-10-06 13:58:47 +0300

    Fix string formatting and keyword filter logic in descript_data_object

--- printo/descript.py
+++ printo/descript.py
@@ -8,24 +8,26 @@ def descript_data_object(
     serializator: Callable[[Any], str] = repr,
     filters: Optional[Dict[Union[str, int], Callable[[Any], bool]]] = None,
 ) -> str:
-    real_filters: Dict[Union[str, int], Callable[[Any], bool]] = filters if filters is not None else {}
+    real_filters: Dict[Union[str, int], Callable[[Any], bool]] = (
+        filters if filters is not None else {}
+    )
 
     args_description_chunks = []
     for index, argument in enumerate(args):
         filter = real_filters.get(index, lambda x: True)
         if filter(argument):
             args_description_chunks.append(serializator(argument))
-    args_description: str = ', '.join(args_description_chunks)
+    args_description: str = ", ".join(args_description_chunks)
 
     kwargs_description_chunks = []
     for argument_name, value in kwargs.items():
         filter = real_filters.get(argument_name, lambda x: True)
-        if filter(argument_name):
-            kwargs_description_chunks.append(f'{argument_name}=' + serializator(value))
-    kwargs_description: str = ', '.join(kwargs_description_chunks)
+        if filter(value):
+            kwargs_description_chunks.append(f"{argument_name}=" + serializator(value))
+    kwargs_description: str = ", ".join(kwargs_description_chunks)
 
-    breackets_content = ', '.join(
+    breackets_content = ", ".join(
         [x for x in (args_description, kwargs_description) if x]
     )
 
-    return f'{class_name}({breackets_content})'
+    return f"{class_name}({breackets_content})"
